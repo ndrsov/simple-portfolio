ad2ea9586596b7f4897426e12bfe07c1ab86a2c3
Andres Soria <andres.zov@gmail.com>
2025-06-13 17:25:39 -0600

    Add navbar items for desktop and mobile  window sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,88 @@
+import { cn } from '@/lib/utils';
+import { Menu, X } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+const navItems = [
+  { name: 'Home', href: '#hero' },
+  { name: 'About', href: '#about' },
+  { name: 'Skills', href: '#skills' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Contact', href: '#contact' },
+];
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.screenY > 10);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  return (
+    <nav
+      className={cn(
+        'fixed w-full z-40 transition-all duration-300',
+        isScrolled ? 'py-3 bg-background/80 backdrop-blur-md shadow-xs' : 'py-5'
+      )}
+    >
+      <div className="container flex items-center justify-between">
+        <a
+          href="#hero"
+          className="text-xl font-bold text-primary flex items-center"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground">@NDRSOV</span> Portfolio
+          </span>
+        </a>
+        {/* Desktop navbar */}
+        <div className="hidden md:flex space-x-8">
+          {navItems.map((item, key) => (
+            <a
+              href="{item.href}"
+              key={key}
+              className="text-foreground/80 hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* Mobile navbar  */}
+
+        <button
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="md:hidden p-2 text-foreground z-50"
+          onClick={() => setisMenuOpen((prev) => !prev)}
+        >
+          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+        <div
+          className={cn(
+            'fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center',
+            'transition-all duration-300 md:hidden',
+            isMenuOpen
+              ? 'opacity-100 pinter-events-auto'
+              : 'opacity-0 pointer-events-none'
+          )}
+        >
+          <div className="flex flex-col space-y-8 text-xl">
+            {navItems.map((item, key) => (
+              <a
+                href="{item.href}"
+                key={key}
+                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                onClick={() => setisMenuOpen(false)}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
